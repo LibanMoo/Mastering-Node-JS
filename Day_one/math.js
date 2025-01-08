96ce5a9b51c898f175050c61b60ab32c01c41bdb
LibanMoo <libanmohamedreal@gmail.com>
2025-01-08 21:05:15 +0300

    This is the day one of Node JS

---
+++ Day_one/math.js
@@ -0,0 +1,9 @@
+// const add = (a,b)=>{
+//     return a + b;
+// }
+// console.log(add(2,2));
+
+// const add = (a, b)=> a + b;
+// module.exports = {add};
+
+exports.Printname = (name)=> console.log(`Your name is ${name}`);
---
+++ Day_one/server.js
@@ -0,0 +1,20 @@
+const os = require('os')
+const { Printname } = require('./math')
+// console.log(os.version());
+// console.log(os.type());
+// console.log(os.homedir());
+// console.log("Hello world");
+
+// console.log(__dirname);
+// console.log(__filename);
+// const path = require('path');
+// console.log(path.parse(__filename));
+
+
+// const math = require('./math')
+// console.log(math.add(2,3));
+
+// const {add} = require('./math');
+// console.log(add(2,5));
+Printname("Liban");
+
