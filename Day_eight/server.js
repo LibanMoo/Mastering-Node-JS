00c85a149aaa5b0373584286de73bb17c1e4aa78
LibanMoo <libanmohamedreal@gmail.com>
2025-04-10 14:54:57 +0300

    installed express and wrote a code that will send response as hello word when its runned

--- Day_eight/server.js
+++ Day_eight/server.js
@@ -3,3 +3,8 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
+app.listen(PORT, ()=> console.log(`app is running on port ${PORT}`));
+
+app.get('/', (req, res)=>{
+    res.send('hello world')
+})
