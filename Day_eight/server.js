da5f84c9ecd69d16f313f6579e29a3e7bf92ca47
LibanMoo <libanmohamedreal@gmail.com>
2025-04-11 11:49:59 +0300

    made a routing that will direct you to the index.html file when a forward is slash is written

--- Day_eight/server.js
+++ Day_eight/server.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.listen(PORT, ()=> console.log(`app is running on port ${PORT}`));
-
-app.get('/', (req, res)=>{
-    // res.send('hello world')
+app.get(/^\/$|\/index\.html$/, (req, res)=>{
+    // res.send('hllo world')
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 app.get('/new-page.html', (req, res)=>{
     // res.send('hello world')
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 })
+
+app.listen(PORT, ()=> console.log(`app is running on port ${PORT}`));
