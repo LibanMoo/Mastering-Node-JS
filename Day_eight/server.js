7354c6ecbdd1b1f621cc11d3ef39447b3a0f7956
LibanMoo <libanmohamedreal@gmail.com>
2025-04-11 12:19:10 +0300

    made the url direct to the index.html file when a foward slash is typed or an index only

--- Day_eight/server.js
+++ Day_eight/server.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.get(/^\/$|\/index\.html$/, (req, res)=>{
+app.get(/^\/$|\/index(.html$)?/, (req, res)=>{
     // res.send('hllo world')
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
-app.get('/new-page.html', (req, res)=>{
+app.get(/^\/new-page(.html$)?/, (req, res)=>{
     // res.send('hello world')
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 })
