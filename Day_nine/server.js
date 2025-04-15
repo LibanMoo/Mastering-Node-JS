94e9d193841afc9d09034b3e352fe048963a05d1
LibanMoo <libanmohamedreal@gmail.com>
2025-04-15 16:50:52 +0300

    made a middleware that surfe the public folder for the index page to acess its css

---
+++ Day_nine/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/static', express.static(path.join(__dirname, 'puplic')));
+
+app.get(/^\/index($.html)?/, (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+
+app.listen(PORT, ()=> console.log(`app is running on port ${PORT}`));
