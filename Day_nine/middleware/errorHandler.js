aa70ec6b1fc0cd4e5652664695d55d07fef67013
LibanMoo <libanmohamedreal@gmail.com>
2025-04-21 20:37:24 +0300

    exported the errorHandler function

--- Day_nine/middleware/errorHandler.js
+++ Day_nine/middleware/errorHandler.js
@@ -1,7 +1,9 @@
 const logEvent = require('./logEvent');
 
-app.use((req, err, res, next)=>{
+const errorHandler = (req, err, res, next)=>{
     logEvent(`${err.name} : ${err.message}`, 'errorLog.txt')
     console.error(err.stack);
     res.status(500).send(err.message);
-})
+}
+
+module.exports = errorHandler;
