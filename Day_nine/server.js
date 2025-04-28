da80a1964a8e56b69ac5c7a7ec8e61820b06357b
LibanMoo <libanmohamedreal@gmail.com>
2025-04-28 22:38:07 +0300

    updated the name of the folder and added get method that will handle index requests and surfed it in the server.js file

--- Day_nine/server.js
+++ Day_nine/server.js
@@ -1,36 +1,13 @@
 const express = require('express');
 const app = express();
-const logEvents = require('./middleware/logEvent');
-const {logger} = require('./middleware/logEvent');
-const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
 
-app.use(logger)
+app.use('/static', express.static(path.join(__dirname, 'puplic')));
 
-const whiteList = [ 'http://127.0.0.1:5500','http://localhost:3500', 'https://www.google.com'];
-const options = {
-    origin: (origin, callback) => {
-        if(whiteList.indexOf(origin)!== -1 || !origin){
-            callback(null, true);
-        }
-        else {
-            callback(new Error('Cors is not allowed for this site'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
-app.use(cors(options));
-
-app.use(express.urlencoded({extended: false}));
-
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/static', express.static(path.join(__dirname, 'puplic')));
+app.use('subdir', require('./routes/subdir'));
 
 app.get(/^\/index($.html)?/, (req, res)=>{
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -40,5 +17,4 @@ app.get(/^.*$/, (req, res)=>{
     res.sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
-app.use(errorHandler);
 app.listen(PORT, ()=> console.log(`app is running on port ${PORT}`));
--- Day_nine/subdir/subdir.js
+++
@@ -1,3 +0,0 @@
-const express = require('express');
-const route = express.Router();
-const path = require('path');
