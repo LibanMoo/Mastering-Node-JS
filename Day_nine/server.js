e1266a863b85202cda226ab661733e06ce80c0a8
LibanMoo <libanmohamedreal@gmail.com>
2025-05-02 11:29:44 +0300

    reverted to get the original after i worked in te wrong folder

--- Day_nine/server.js
+++ Day_nine/server.js
@@ -5,9 +5,29 @@ const path = require('path');
 const PORT = process.env.PORT || 3500;
 
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(logger)
 
-app.use('subdir', require('./routes/subdir'));
+const whiteList = [ 'http://127.0.0.1:5500','http://localhost:3500', 'https://www.google.com'];
+const options = {
+    origin: (origin, callback) => {
+        if(whiteList.indexOf(origin)!== -1 || !origin){
+            callback(null, true);
+        }
+        else {
+            callback(new Error('Cors is not allowed for this site'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(options));
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/static', express.static(path.join(__dirname, 'puplic')));
 
 app.get(/^\/index($.html)?/, (req, res)=>{
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
