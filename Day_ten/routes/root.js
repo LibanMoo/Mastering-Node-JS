97807bba77910ebe2ab405e9936e42fa1a1e32a8
LibanMoo <libanmohamedreal@gmail.com>
2025-05-02 11:43:28 +0300

    moved the root handlers in here for more clean code

--- Day_ten/routes/root.js
+++ Day_ten/routes/root.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const route = express.Router();
 const path = require('path');
+
+route.get(/^\/index($.html)?/, (req, res)=>{
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+})
+route.get(/^\/new-page($.html)?/, (req, res)=>{
+    res.sendFile(path.join(__dirname, '..', 'views', 'new-page.html'));
+})
+route.get(/^\/new-page($.html)?/, (req, res)=>{
+    res.redirect(301, path.join(__dirname, '..', 'views', 'new-page.html'));
+})
