b345428c0c1dda60e581e5edcdf2d0471a6807e7
LibanMoo <libanmohamedreal@gmail.com>
2025-05-02 21:43:30 +0300

    this is Day_eleven of mastering Node Js and today its abot mvc restful apis

---
+++ Day_eleven/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/employees', require('./routes/api/employees'))
+
+app.use('/', require('./routes/root'))
+
+app.use('subdir', require('./routes/subdir'));
+
+app.get(/^.*$/, (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(PORT, ()=> console.log(`app is running on port ${PORT}`));
