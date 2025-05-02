f5ff9ad707addd540ac3c8c0d6a6498d366eaf89
LibanMoo <libanmohamedreal@gmail.com>
2025-05-02 22:30:30 +0300

    created four functions to handle CRUD operations and mantain clean code

--- Day_eleven/controllers/employeesController.js
+++ Day_eleven/controllers/employeesController.js
@@ -1,2 +1,21 @@
 const data = {};
-data.employee = require('../model/employees.json')
+data.employees = require('../model/employees.json')
+
+const getAllEmployee = (req, res) =>{
+    res.json(data.employees)
+}
+ 
+const getEmployee = (req, res) => {
+    res.json({"id": req.params.id})
+}
+
+const updateEmployee = (req, res) => {
+    res.json({
+        "firstName": req.body.firsName,
+        "lastName": req.body.lastName
+    })
+}
+
+const deleteEmployee = (req, res) => {
+    res.json({"id": req.params.id})
+}
