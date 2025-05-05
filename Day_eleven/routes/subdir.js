085917e8099130e047614d2325cae16442690045
LibanMoo <libanmohamedreal@gmail.com>
2025-05-06 00:00:36 +0300

    this is my request logs

--- Day_eleven/routes/subdir.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const route = express.Router();
-const path = require('path');
-
-route.get(/^\/index($.html)?/, (req, res)=>{
-      res.sendFile(path.join(__dirname, '..', 'views', 'subdir', 'index.html'));
-})
-
-module.exports = route;
