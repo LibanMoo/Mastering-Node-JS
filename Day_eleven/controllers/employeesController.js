4407bd931c9b91ec7587e764fee8c6f2a5bb93d0
LibanMoo <libanmohamedreal@gmail.com>
2025-05-08 22:21:24 +0300

    created a function that will create newEmployees dynamically

--- Day_eleven/controllers/employeesController.js
+++ Day_eleven/controllers/employeesController.js
@@ -6,10 +6,17 @@ const getAllEmployee = (req, res) =>{
 }
 
 const createEmployee = (req, res) => {
-    res.json({
-        "firstName": req.body.firstName,
-        "lastName": req.body.lastName
-    })
+   const newEmployee = {
+    id: data.employees(data.employees.length -1).data || 1,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+   }
+   if(!newEmployee.firstName || !newEmployee.lastName){
+    res.status(400).json({"message": "No data found"});
+   }
+   data.setEmployee([...data.employees, newEmployee]);
+   res.json(data.employees);
+
 }
 const getEmployees = (req, res) => {
     res.json({"id": req.params.id})
