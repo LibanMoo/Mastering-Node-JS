4eb89ab616aefb17b1ef7a17bc028cf03856d9cb
LibanMoo <libanmohamedreal@gmail.com>
2025-05-09 18:10:43 +0300

    removed the data object since its unneccessary

--- Day_eleven/routes/api/employees.js
+++ Day_eleven/routes/api/employees.js
@@ -2,10 +2,7 @@ const express = require('express');
 const { post } = require('../root');
 const employeesController = require('../../controllers/employeesController');
 const router = express.Router();
-const data = {};
-
-data.employees = require('../../model/employees.json');
-
+console.log('reached here too')
 router.route('/')
       .get(employeesController.getAllEmployee)
       .post(employeesController.createEmployee)
