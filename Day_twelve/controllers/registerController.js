2dd643329f0f0a36a311775d34a5fb261f21c2bc
LibanMoo <libanmohamedreal@gmail.com>
2025-05-12 00:02:35 +0300

    fininshed the registeration function

--- Day_twelve/controllers/registerController.js
+++ Day_twelve/controllers/registerController.js
@@ -15,6 +15,29 @@ const handleNewUser = async (req, res)=>{
     if (!user || !pwd){
      res.status(400).json({"message": "Username and Password needed"})
     }
+    const dublicate = data.user.find(person => person.username = user);
+
+    if (dublicate){
+        res.status(409).json({"message": "this user exits"})
+    }
+    try{
+      const hashed_pw = await bcrypt.hash(pwd, 10);
+
+      const newUser = {username: user, password: hashed_pw};
+
+      data.setUser(...data.user, newUser);
+
+      await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(data.user)
+      )
+      console.log(`new user: ${user} and password: ${hashed_pw}`)
+      res.status(201).json({"success": "User info recorded succesfully"})
+}
+    
+    catch(err){
+         console.error(err);
+    }
 }
 
 module.exports = {handleNewUser};
