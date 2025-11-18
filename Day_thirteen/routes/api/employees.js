f93244703f7af55eb0e9cc2709d4405334121f9d
healpluspc1 <libanmohamedreal@gmail.com>
2025-11-18 22:30:57 +0300

    feat: made the get all employee request protected using the verifyJWT middleware

--- Day_thirteen/routes/api/employees.js
+++ Day_thirteen/routes/api/employees.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const { post } = require('../root');
-const employeesController = require('../../controllers/employeesController');
+const express = require("express");
+const { post } = require("../root");
+const employeesController = require("../../controllers/employeesController");
+const verifyJWT = require("../../middleware/verfiyJWT");
 const router = express.Router();
-console.log('reached here too')
-router.route('/')
-      .get(employeesController.getAllEmployee)
-      .post(employeesController.createEmployee)
-      .put(employeesController.updateEmployee)
-      .delete(employeesController.deleteEmployee);
+console.log("reached here too");
+router
+  .route("/")
+  .get(verifyJWT, employeesController.getAllEmployee)
+  .post(employeesController.createEmployee)
+  .put(employeesController.updateEmployee)
+  .delete(employeesController.deleteEmployee);
 
-      router.route('/:id')
+router
+  .route("/:id")
 
-          .get(employeesController.getEmployees)
+  .get(employeesController.getEmployees);
 module.exports = router;
