9f5ee161005ffd4624cd19ca934fa9f9d8d7b79c
healpluspc1 <libanmohamedreal@gmail.com>
2025-11-19 11:40:37 +0300

    chore: removed the protection of the get request since it's not necessary

--- Day_thirteen/routes/api/employees.js
+++ Day_thirteen/routes/api/employees.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const { post } = require("../root");
 const employeesController = require("../../controllers/employeesController");
-const verifyJWT = require("../../middleware/verfiyJWT");
 const router = express.Router();
-console.log("reached here too");
 router
   .route("/")
-  .get(verifyJWT, employeesController.getAllEmployee)
+  .get(employeesController.getAllEmployee)
   .post(employeesController.createEmployee)
   .put(employeesController.updateEmployee)
   .delete(employeesController.deleteEmployee);
