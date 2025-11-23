e6be0d7c7af0532fa35e8fcfb02b40762819b1cb
healpluspc1 <libanmohamedreal@gmail.com>
2025-11-23 12:19:30 +0300

    chore: used the same credentials as registering when deleting one

--- Day_thirteen/controllers/logoutController.js
+++ Day_thirteen/controllers/logoutController.js
@@ -5,28 +5,35 @@ const usersData = {
   },
 };
 
-const fsPromises = require('fs').promises;
-const path = require('path');
+const fsPromises = require("fs").promises;
+const path = require("path");
 
 const handleLogout = async (req, res) => {
-    const cookies = req.cookies;
-    
-    if (!cookies?.jwt) return res.sendStatus(204);
-    const refreshToken = cookies.jwt;
+  const cookies = req.cookies;
 
-    const foundUser = usersData.users.filter(person => person.refreshToken === refreshToken);
-    if(!foundUser) return res.sendStatus(403);
+  if (!cookies?.jwt) return res.sendStatus(204);
+  const refreshToken = cookies.jwt;
 
-    const otherUsers = usersData.users.filter(
-      (person) => person.refreshToken !== refreshToken
-    );
-    const currentUser = {...foundUser, refreshToken: ''};
-    usersData.setUser([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.json'),
-        JSON.stringify(usersData.users)
-    )
-    res.clearCookie('jwt', {httpOnly: true, maxAge: 24 * 60 * 60 * 1000});
-    res.sendStatus(204);
-}
-module.exports = {handleLogout};
+  const foundUser = usersData.users.filter(
+    (person) => person.refreshToken === refreshToken
+  );
+  if (!foundUser) return res.sendStatus(403);
+
+  const otherUsers = usersData.users.filter(
+    (person) => person.refreshToken !== refreshToken
+  );
+  const currentUser = { ...foundUser, refreshToken: "" };
+  usersData.setUser([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "model", "users.json"),
+    JSON.stringify(usersData.users)
+  );
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+  res.sendStatus(204);
+};
+module.exports = { handleLogout };
