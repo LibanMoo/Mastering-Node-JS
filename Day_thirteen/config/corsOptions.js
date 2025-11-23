2ad74dd01dcd22cfa942de959a4e51b64c11edca
healpluspc1 <libanmohamedreal@gmail.com>
2025-11-23 12:17:27 +0300

    feat: used the allowedOrigin data to allows only requests that are coming from allowed origins

--- Day_thirteen/config/corsOptions.js
+++ Day_thirteen/config/corsOptions.js
@@ -1,6 +1,13 @@
-const whiteList = [ 'http://127.0.0.1:5500','http://localhost:3500', 'https://www.google.com'];
+const allowedOrigins = require('./allowedOrigins');
 const corsOptions = {
-    origin: "*",
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin ){
+            callback(null, true)
+        }
+        else {
+            callback(new Error('request is not allowed'));
+        }
+    },
     optionsSuccessStatus: 200
 }
 
