519955e02ff08ce8d412b8781493949b105ff2ff
healpluspc1 <libanmohamedreal@gmail.com>
2025-11-29 19:04:54 +0300

    feat: added a new object when registering user Info in the access token generation

--- Day_fourteen/controllers/authController.js
+++ Day_fourteen/controllers/authController.js
@@ -25,7 +25,11 @@ const handleLogin = async (req, res) => {
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
     const accessToken = jwt.sign(
-      { "username": foundUser.username },
+      { "userInfo": {
+           "username": foundUser.username,
+           "roles": foundUser.roles
+      },
+    },
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "60s" }
     );
