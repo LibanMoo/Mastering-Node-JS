bf9079e1a0f62d1916d4cf37a3878e390ccf3b39
healpluspc1 <libanmohamedreal@gmail.com>
2025-12-05 20:43:42 +0300

    this is Day fifteen of Mastering Node JS and today it will about intro to MongoDB

---
+++ Day_fifteen/controllers/authController.js
@@ -0,0 +1,62 @@
+const usersData = {
+  users: require("../model/users.json"),
+  setUser: function (data) {
+    this.users = data;
+  },
+};
+const path = require('path');
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "username and password are required" });
+  const foundUser = usersData.users.find((person) => person.username === user);
+
+  if (!foundUser) return res.sendStatus(401);
+
+  const match = await bcrypt.compare(pwd, foundUser.password);
+  if (match) {
+    const accessToken = jwt.sign(
+      { "userInfo": {
+           "username": foundUser.username,
+           "roles": foundUser.roles
+      },
+    },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "60s" }
+    );
+    const refreshToken = jwt.sign(
+      { "username": foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    const otherUsers = usersData.users.filter(
+      (person) => person !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+
+    usersData.setUser([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersData.users)
+    );
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ accessToken });
+  } else {
+    res.sendStatus(401);
+  }
+};
+module.exports = { handleLogin };
---
+++ Day_fifteen/controllers/employeesController.js
@@ -0,0 +1,62 @@
+const data = {
+    employees : require('../model/employees.json'),
+    setEmployees: function (data) { this.employees = data}
+};
+
+
+const getAllEmployee = (req, res) =>{
+    res.json(data.employees)
+}
+
+const createEmployee = (req, res) => {
+   const newEmployee = {
+    id: data.employees?.length ? data.employees[data.employees.length -1].id +1 : 1,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+   }
+   if(!newEmployee.firstName || !newEmployee.lastName){
+    res.status(400).json({"message": "Set First name and Last name"});
+   }
+   data.setEmployees([...data.employees, newEmployee]);
+   res.status(201).json(data.employees);
+
+}
+const getEmployees = (req, res) => {
+   const employee = data.employees.find((emp)=> emp.id === parseInt(req.params.id));
+   if (!employee){
+    return res.status(500).json({"message": `this employee ${req.params.id} doesnt exist`})
+   }
+   res.json(employee)
+}
+
+const updateEmployee = (req, res) => {
+  const employee = data.employees.find((emp)=> emp.id === parseInt(req.body.id));
+  if(!employee){
+    res.status(400).json({"message": `Employee ID ${req.body.id} not found`})
+  };
+  
+  if (req.body.firstName) employee.firstName = req.body.firstName;
+  if(req.body.lastName) employee.lastName = req.body.lastName;
+  const filteredArray = data.employees.filter((emp)=> emp.id !== parseInt(req.body.id))
+  const unsortedArray = [...filteredArray, employee];
+  data.setEmployees(unsortedArray.sort((a, b)=> a.id > b.id ? 1 : a.id < b.id ? -1 :0))
+  res.status(201).json(data.employees);
+}
+
+const deleteEmployee = (req, res) => {
+    const employee = data.employees.find((emp)=> emp.id === parseInt(req.body.id))
+    if (!employee){
+        res.status(500).json({"message": `Employee ${req.body.id} not found`})
+    };
+   const filteredArray = data.employees.filter((emp) => emp.id !== parseInt(req.body.id))
+   data.setEmployees(...filteredArray);
+   res.status(201).json(data.employees)
+}
+
+module.exports = {
+    getAllEmployee,
+    getEmployees,
+    updateEmployee,
+    deleteEmployee,
+    createEmployee
+};
