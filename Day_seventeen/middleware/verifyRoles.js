552ee76a7524a9ed5e23cb5260cad16e2b8b1bf6
healpluspc1 <libanmohamedreal@gmail.com>
2025-12-14 20:44:54 +0300

    fix: changed the roles to array when originally were objects

--- Day_seventeen/middleware/verifyRoles.js
+++ Day_seventeen/middleware/verifyRoles.js
@@ -1,16 +1,16 @@
-const userRoles = require("../config/roles");
-
 const verifyRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    if (!req?.roles) return res.sendStatus(401);
-    if (!Array.isArray(req.roles)) {
-      req.roles = Object.values(req.roles);
-    }
-    const rolesList = [...allowedRoles];
-    const result = req.roles
-      .map((role) => rolesList.includes(role))
-      .find((val) => val === true);
-    if (!result) return res.sendStatus(401);
+    if (!req?.roles) return res.sendStatus(402);
+    // Convert { User:2001, Editor:1984, Admin:5150 } → [2001, 1984, 5150]
+    const userRoles = Array.isArray(req.roles)
+      ? req.roles
+      : Object.values(req.roles);
+
+    // Check if user has at least one allowed role
+    const hasRole = userRoles.some((role) => allowedRoles.includes(role));
+
+    if (!hasRole) return res.sendStatus(401);
+
     next();
   };
 };
